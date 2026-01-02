8ed3efb96d95b9510e301224eb558c36eceea7bb
Frehitzz <fritzharlydegamo@gmail.com>
2026-01-02 12:30:46 +0800

    feat: change the programtool, framtool and education to mapping array, and done styling for phone size

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -3,10 +3,51 @@ import { FaCss3 } from "react-icons/fa";
 import { FaJs } from "react-icons/fa";
 import { RiPhpFill } from "react-icons/ri";
 import { GrMysql } from "react-icons/gr";
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaGitAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { VscVscode } from "react-icons/vsc";
+import { FaFigma } from "react-icons/fa";
+
 function Aboutme() {
+  const programLang = [
+    { id: 1, icon: FaHtml5 },
+    { id: 2, icon: FaCss3 },
+    { id: 2, icon: FaJs },
+    { id: 2, icon: RiPhpFill },
+    { id: 2, icon: GrMysql },
+  ];
+
+  const frameTools = [
+    { id: 1, icon: FaReact },
+    { id: 2, icon: RiTailwindCssFill },
+    { id: 3, icon: FaGitAlt },
+    { id: 4, icon: FaGithub },
+    { id: 5, icon: VscVscode },
+    { id: 6, icon: FaFigma },
+  ];
+
+  const education = [
+    {
+      id: 1,
+      level: "HIGH SCHOOL",
+      name: "Army's Angel Integrated School",
+      year: "2020 - 2022",
+    },
+    {
+      id: 2,
+      level: "COLLEGE",
+      name: "Rizal Technological University",
+      year: "2022 - PRESENT",
+    },
+  ];
+
   return (
     <div>
-      <h1 className="text-center text-2xl mt-8">ABOUT ME</h1>
+      <h1 className="text-center text-3xl mt-15 text-[#91ff00] text-bold">
+        ABOUT ME
+      </h1>
 
       <div className="grid grid-cols-1 grid-rows-3 gap-4 mt-5">
         {/* SHORT DESCRIPTION CONTAINER */}
@@ -28,18 +69,16 @@ function Aboutme() {
           <div className="scroll-container flex-1 flex items-center">
             <div className="inline-flex text-5xl gap-4 text-white">
               <div className="scroll-left inline-flex gap-4">
-                <FaHtml5 />
-                <FaCss3 />
-                <FaJs />
-                <RiPhpFill />
-                <GrMysql />
+                {programLang.map((item) => {
+                  const Icon = item.icon;
+                  return <Icon key={item.id} />;
+                })}
               </div>
               <div className="scroll-left inline-flex gap-4">
-                <FaHtml5 />
-                <FaCss3 />
-                <FaJs />
-                <RiPhpFill />
-                <GrMysql />
+                {programLang.map((item) => {
+                  const Icon = item.icon;
+                  return <Icon key={`duplicate-${item.id}`} />;
+                })}
               </div>
             </div>
           </div>
@@ -52,23 +91,35 @@ function Aboutme() {
           <div className="scroll-container flex-1 flex items-center">
             <div className="inline-flex text-5xl gap-4 text-white">
               <div className="scroll-left inline-flex gap-4">
-                <FaHtml5 />
-                <FaCss3 />
-                <FaJs />
-                <RiPhpFill />
-                <GrMysql />
+                {frameTools.map((item) => {
+                  const Icon = item.icon;
+                  return <Icon key={item.id} />;
+                })}
               </div>
               <div className="scroll-left inline-flex gap-4">
-                <FaHtml5 />
-                <FaCss3 />
-                <FaJs />
-                <RiPhpFill />
-                <GrMysql />
+                {frameTools.map((item) => {
+                  const Icon = item.icon;
+                  return <Icon key={`duplicate-${item.id}`} />;
+                })}
               </div>
             </div>
           </div>
         </div>
-        <div className="rounded-lg bg-pink-300">EDUCATION</div>
+        {/* EDUCATION CONTAINER */}
+        <div className="min-h-40 rounded-lg flex flex-col p-2 border border-[#91ff00]">
+          <p className="text-[#91ff00] text-lg font-bold text-center">
+            EDUCATION
+          </p>
+          <div className="flex justify-between mt-3">
+            {education.map((edu) => (
+              <div key={edu.id} className="flex flex-col items-center">
+                <h1 className="text-[#599c02]">{edu.level}</h1>
+                <p className="text-center text-white">{edu.name}</p>
+                <p className="text-[#91ff00]">{edu.year}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
 
       {/* DESKTOP GRID */}
