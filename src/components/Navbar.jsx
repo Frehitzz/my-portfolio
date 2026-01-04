8f268a70c529bbe70dcdffe40a99218c6fb81bb8
Frehitzz <fritzharlydegamo@gmail.com>
2026-01-04 22:28:54 +0800

    refactor-feat: uses an array mapping then creating an active navitem color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 function Navbar() {
+  const [active, setActive] = useState("Home");
+
+  const navItems = [
+    { name: "Projects", link: "../pages/Home.jsx" },
+    { name: "Home", link: "../pages/Home.jsx" },
+    { name: "Contacts", link: "../pages/Home.jsx" },
+  ];
+
   return (
     <div className="flex mt-5">
       <ul className="flex items-center justify-center w-full gap-12 text-lg md:text-2xl lg:text-3xl text-white">
-        <li>
-          <a href="../pages/Home.jsx">Home</a>
-        </li>
-        <li>
-          <a href="#">Projects</a>
-        </li>
-        <li>
-          <a href="#">Contacts</a>
-        </li>
+        {navItems.map((item) => (
+          <li
+            key={item.name}
+            className={` ${active === item.name ? "text-[#91ff00]" : ""}`}
+            onClick={() => setActive(item.name)}
+          >
+            <a href={item.link}>{item.name}</a>
+          </li>
+        ))}
       </ul>
     </div>
   );
