f03c8282d2716d6a3742c397a4a486f280ccb748
Frehitzz <fritzharlydegamo@gmail.com>
2026-01-08 21:02:32 +0800

    feat: added contact page only ui no backend, done styling for mobile view

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,83 @@
+import { MdOutlineEmail } from "react-icons/md";
+import { FaRegCopy } from "react-icons/fa6";
+import { useState } from "react";
 function Contact() {
+  const [copied, setCopy] = useState(false);
+
+  // COPY EMAIL
+  const copy = () => {
+    navigator.clipboard.writeText("fritzharlydegamo@gmail.com");
+    setCopy(true);
+    setTimeout(() => setCopy(false), 2000);
+  };
+
   return (
-    <div className="">
-      <h1 className="text-5xl">THIS IS CONTACT</h1>
+    <div className="mt-5 px-4">
+      {/* email copy notifications */}
+      <div className="w-auto flex justify-center">
+        <span
+          className={`text-center border border-white/[0.5] p-1 w-50 text-white/[0.5] rounded-2xl transition-all duration-300 ease-out ${
+            copied ? "opacity-100 scale-100" : "opacity-0 scale-95"
+          }`}
+        >
+          Copied
+        </span>{" "}
+      </div>
+      <h1 className="text-2xl py-4 text-[#91ff00] text-center">Get in touch</h1>
+      {/* FORM */}
+      <form className="">
+        <div className="flex flex-col gap-4 text-white">
+          {/* name */}
+          <div className="">
+            <label className="text-lg">Name</label>
+            <br />
+            <input
+              className="font-light text-white/[0.5] outline-none p-2 border border-white/[0.2] w-full rounded"
+              type="text"
+              placeholder="Your Identity"
+              required
+            ></input>
+          </div>
+          {/* email */}
+          <div className="">
+            <label className="text-lg">Email</label>
+            <br />
+            <input
+              className="font-light text-white/[0.5] outline-none p-2 border border-white/[0.2] w-full rounded"
+              type="text"
+              placeholder="Your email is safe here"
+              required
+            ></input>
+          </div>
+        </div>
+        {/* content mesage */}
+        <div className="py-4">
+          <label className="text-lg text-white">Content</label>
+          <br />
+          <textarea
+            className="font-light text-white/[0.5] outline-none p-2 w-full h-40 border border-white/[0.2] rounded"
+            placeholder="Place your message here..."
+          ></textarea>
+        </div>
+        {/* button */}
+        <div className="">
+          <button className="w-full bg-[#91ff00] p-2 outline-none border border-[#599c02] rounded text-lg font-semibold">
+            Send Message
+          </button>
+        </div>
+      </form>
+      {/* horizontal line */}
+      <hr className="text-white/[0.2] my-5" />
+      {/* copy email */}
+      <div className="flex text-lg gap-2 items-center justify-center text-white border border-white/[0.2] p-2 rounded-2xl">
+        <p>fritzharlydegamo@gmail.com</p>
+        <button
+          className="p-1 border border-white/[0.5] rounded"
+          onClick={copy}
+        >
+          <FaRegCopy />
+        </button>
+      </div>
     </div>
   );
 }
